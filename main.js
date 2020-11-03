532c559caff79fde34baad291fb083764e4f94c2
victorwikstrom <victor.wikstrom@icloud.com>
2020-11-03 13:40:52 +0100

    Add button to switch between next and previous image

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-window.onload = main
-//window.addEventListener('load', main())
 
-function main() {
-  startSliderAnimation()
+const images = document.querySelectorAll('.img')
+const container = document.getElementById('container')
+const nextButton = document.getElementById('next-button')
+const previousButton = document.getElementById('previous-button')
+
+nextButton.addEventListener('click', showNextImage)
+previousButton.addEventListener('click', showPreviousImage)
+
+function showPreviousImage() {
+  for (let i = 0; i < images.length; i++)
+  if (images[i].classList.contains('show')) {
+    images[i].classList.remove('show')
+
+    if (i === 0) {
+      lastIndex = images.length - 1
+      images[0].classList.remove('show')
+      images[lastIndex].classList.add('show')
+    } 
+    else {
+      nextIndex = i - 1
+      images[nextIndex].classList.add('show')
+    }
+
+    break
+  }
 }
 
-function startSliderAnimation() {
-  setInterval(changeActiveImage, 3000)
-}
-
-function changeActiveImage() {
-  const images = document.querySelectorAll('.img')
-  console.log(images)
-
+function showNextImage() {
   for (let i = 0; i < images.length; i++) {
-    console.log(i)
     if (images[i].classList.contains('show')) {
       images[i].classList.remove('show')
 
       nextIndex = (i + 1) % images.length
       images[nextIndex].classList.add('show')
+
       break
     }
 
