3d8809450b39f67e9a1428eb46e3828a74f28850
victorwikstrom <victor.wikstrom@icloud.com>
2020-11-14 18:31:55 +0100

    Add working slideshow indicators

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', () => initSlider())
 
 const initSlider = () => {
-  const slideList = getSlides('slide') // Add classname of slide
+  const slideList = getSlides('slide') // Add classname for slide
+  const indicators = getIndicators(slideList, 'indicator-container', 'indicator')
+  
   let currentSlide = 0
   slideList[currentSlide].style.opacity = 1
+  indicators[currentSlide].style.opacity = 1
   
   nextButton = document.getElementById('next-btn')
   prevButton = document.getElementById('prev-btn')
+
 
   nextButton.addEventListener('click', showNextSlide = () => {
     let nextSlide = currentSlide + 1 
@@ -14,17 +18,21 @@ const initSlider = () => {
       nextSlide = 0
     }
     slideList[currentSlide].style.opacity = 0
+    indicators[currentSlide].style.opacity = 0.5
     slideList[nextSlide].style.opacity = 1
+    indicators[nextSlide].style.opacity = 1
     currentSlide = nextSlide
   })
-
+  
   prevButton.addEventListener('click', showPrevSlide = () => {
     let prevSlide = currentSlide - 1
     if (currentSlide === 0) {
       prevSlide = slideList.length - 1
     }
     slideList[currentSlide].style.opacity = 0
+    indicators[currentSlide].style.opacity = 0.5
     slideList[prevSlide].style.opacity = 1
+    indicators[prevSlide].style.opacity = 1
     currentSlide = prevSlide
   })
 }
@@ -32,6 +40,18 @@ const initSlider = () => {
 const getSlides = (slidesId) => {
   const slides = document.getElementsByClassName(slidesId)
   return slides
+}
+const getIndicators = (slides, indicatorContainerId, indicatorClassName) => {
+  container = document.getElementById(indicatorContainerId)
+  const indicators = []
+  
+  for (let slide of slides) {
+    const indicator = document.createElement('div')
+    indicator.classList.add(indicatorClassName)
+    container.appendChild(indicator)
+    indicators.push(indicator)
+  }
+  return indicators
 }
 
 
